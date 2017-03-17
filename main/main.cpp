e9fac6abec8de44a448b15979c556d7f61e7e199
fallahn <matty_styles@hotmail.com>
2017-03-17 12:49:33 +0000

    add main entry point project

---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <SDL.h>
+
+int main(int argc, char** argsv)
+{
+	//put your SDL / game code here
+
+
+	return 0;
+}
---
+++ main/main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class main
+{
+public:
+	const char * getPlatformABI();
+	main();
+	~main();
+};
+
