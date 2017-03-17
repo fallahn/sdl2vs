e9c7b1b3b9748156ffd007849a76f616dab0e76d
fallahn <matty_styles@hotmail.com>
2017-03-17 12:06:39 +0000

    add SDL2 java application

---
+++ SDL2_Application/src/com/SDL2_Application/SDL2_Application.java
@@ -0,0 +1,15 @@
+
+package com.SDL2_Application;
+
+import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
+
+public class SDL2_Application extends SDLActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
